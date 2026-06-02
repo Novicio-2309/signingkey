a5e3324c308cb2df9f1ac115fa0ddd43e948dda6
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-06-02 18:17:20 +0800

    Update development certificate paths in keys.mk

--- keys.mk
+++ keys.mk
@@ -1,5 +1,5 @@
 ifeq ($(strip $(PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity-priv/keys/releasekey
-    PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
-    PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
+    PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/lineage-priv/keys
+    PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/lineage-priv/keys
 endif
