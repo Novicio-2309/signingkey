f81146f5c721b07742e23baf5d45d15eda7f0dd0
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-04 21:05:03 +0800

    Remove conditional assignment for dev certificates

--- keys.mk
+++ keys.mk
@@ -1,5 +1,1 @@
-ifeq ($(strip $(PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
-    PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/lineage-priv/keys
-    PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/lineage-priv/keys
-endif
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
