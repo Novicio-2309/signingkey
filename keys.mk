e0bc8f2801266d70fa7c5ffcb5a6b6d4a8a43dbd
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-14 21:00:40 +0800

    Update paths for development certificates

--- keys.mk
+++ keys.mk
@@ -1,3 +1,3 @@
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/yaap/signing/keys/releasekey
-PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/yaap/signing/keys
-PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/yaap/signing/keys
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity-priv/keys/releasekey
+PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
+PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
