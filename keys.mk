9dc405fa94bedba079d2c5c798d8009e4c8e6ec7
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-09-05 21:04:22 +0800

    Update paths for development certificates

--- keys.mk
+++ keys.mk
@@ -1,3 +1,3 @@
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/infinity-priv/keys/releasekey
-PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
-PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/infinity-priv/keys
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/xperience/signing/keys/releasekey
+PRODUCT_MAINLINE_SEPOLICY_DEV_CERTIFICATES := vendor/xperience/signing/keys
+PRODUCT_MAINLINE_BLUETOOTH_SEPOLICY_DEV_CERTIFICATES := vendor/xperience/signing/keys
